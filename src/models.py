5134e918c42b21d77a39c350d93a5e17d4b3f57f
PodYapolsky <podyapolskiyaa@gmail.com>
2024-08-15 00:17:02 +0300

    ✨ feat: add CRUD with mongodb

--- src/models.py
+++ src/models.py
@@ -3,30 +3,9 @@ from pydantic import BaseModel
 
 
 class Transaction(BaseModel):
-    id: int
     date: datetime
     is_income: bool
     amount: float
     description: str | None
     category: str
     subcategory: str
-
-
-class TransactionCreateResponse(BaseModel):
-    id: int
-    message: str
-
-
-class TransactionReadResponse(BaseModel):
-    id: int
-    message: str
-
-
-class TransactionUpdateResponse(BaseModel):
-    id: int
-    message: str
-
-
-class TransactionRemoveResponse(BaseModel):
-    id: int
-    message: str
--- src/routers/transactions.py
+++ src/routers/transactions.py
@@ -1,40 +1,62 @@
+import bson
 from fastapi import APIRouter, HTTPException
-from src.models import (
-    Transaction,
-    TransactionCreateResponse,
-    TransactionReadResponse,
-    TransactionUpdateResponse,
-    TransactionRemoveResponse,
-)
 
-db = []
+from src.mongo import db
+from src.models import Transaction
+
 router = APIRouter(prefix="/transactions")
 
 
-@router.get("/")
-async def get_transactions():
-    return {}
+@router.get("/", response_model=list[Transaction])
+async def get_transactions(limit: int = 10):
+    transactions = await db.transactions.find().to_list(length=limit)
+    return transactions
 
 
-@router.post("/")
+@router.post("/", response_model=Transaction)
 async def create_transaction(transaction: Transaction):
-    return TransactionCreateResponse(id=..., message="Transaction added")
+    result = await db.transactions.insert_one(transaction.model_dump())
+    if not result.acknowledged:
+        return HTTPException(status_code=400, detail="Failed to create transaction")
+    return transaction
 
 
-@router.get("/{transaction_id}")
-async def read_transaction(transaction_id: int):
-    return TransactionReadResponse(id=transaction_id, message="Transaction read")
+@router.get("/{transaction_id}", response_model=Transaction)
+async def read_transaction(transaction_id: str):
+    if not bson.ObjectId.is_valid(transaction_id):
+        return HTTPException(
+            status_code=404, detail="Invalid transaction ID, not a bson.ObjectId"
+        )
+
+    transaction = await db.transactions.find_one({"_id": bson.ObjectId(transaction_id)})
+    if not transaction:
+        return HTTPException(status_code=400, detail="Transaction not found")
+    return transaction
+
+
+@router.put("/{transaction_id}", response_model=Transaction)
+async def update_transaction(transaction_id: str, transaction_data: Transaction):
+    if not bson.ObjectId.is_valid(transaction_id):
+        return HTTPException(
+            status_code=404, detail="Invalid transaction ID, not a bson.ObjectId"
+        )
+
+    transaction = await db.transactions.find_one_and_update(
+        filter={"_id": bson.ObjectId(transaction_id)},
+        update={"$set": transaction_data.model_dump()},
+    )
+    return transaction
 
 
 @router.delete("/{transaction_id}")
-async def remove_transaction(transaction_id: int):
-    return TransactionRemoveResponse(id=transaction_id, message="Transaction removed")
-
-
-@router.put("/{transaction_id}")
-async def update_transaction(transaction_id: int, transaction_data: Transaction):
-    if transaction_data:
-        return TransactionUpdateResponse(
-            id=transaction_id, message="Transaction updated"
+async def remove_transaction(transaction_id: str):
+    if not bson.ObjectId.is_valid(transaction_id):
+        return HTTPException(
+            status_code=404, detail="Invalid transaction ID, not a bson.ObjectId"
         )
-    return HTTPException(status_code=404, detail="Transaction not found")
+
+    result = await db.transactions.delete_one({"_id": bson.ObjectId(transaction_id)})
+    if not result.acknowledged:
+        return HTTPException(status_code=400, detail="Failed to delete transaction")
+
+    return {"message": f"Transaction {transaction_id} deleted"}
